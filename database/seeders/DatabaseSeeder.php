5356858db4c7f947f0313c50a39dfe817d77818b
Diego Santana <3c.santana@gmail.com>
2026-03-20 19:25:19 -0300

    First commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('senha1234')
+        ]);
+
+        User::factory()->create([
+            'name' => 'admin-teste',
+            'email' => 'admin-teste@email.com',
+            'password' => 'senha1234'
+        ]);
+    }
+}
